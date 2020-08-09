6d3a682220ae36a36feeaca887545ad428473492
Samuel Knepper <sambobkne@gmail.com>
2020-08-09 13:22:00 -0500

    package npm

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+interface HelloWorldResult {
+    content?: string;
+    id?: string;
+}
+
 function App() {
+    const [content, setContent] = useState<HelloWorldResult>({});
+
+    useEffect(() => {
+        async function fetchData() {
+            const result = await fetch('/greeting');
+
+            setContent(await result.json());
+        }
+
+        fetchData();
+    }, []);
+
     return <div className="App">
-        <p>Hello, World</p>
+        <p>{`${content.content}, ${content.id}`}</p>
     </div>
 }
 
